29ade36bccdaa129b6bd3d841c7ae902d139da0b
AndersonAbreuJr <68236072+AndersonAbreuJr@users.noreply.github.com>
2020-09-22 14:17:54 -0400

    Updates

---
+++ precache-manifest.f551538068ea7a3ee91ac27bcda7e7aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a238032666dd157bba59829a4d1be7e4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "901f8143036f8ba64890",
+    "url": "/static/css/main.9ac0a079.chunk.css"
+  },
+  {
+    "revision": "3230355ccdf577a326bd",
+    "url": "/static/js/2.f4929f23.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f4929f23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "901f8143036f8ba64890",
+    "url": "/static/js/main.ca2c1342.chunk.js"
+  },
+  {
+    "revision": "4692a822c446ee922012",
+    "url": "/static/js/runtime-main.00baba5d.js"
+  }
+]);
